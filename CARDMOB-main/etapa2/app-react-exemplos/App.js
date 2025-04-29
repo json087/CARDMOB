8de202689a35a9832daa76b0a8d2c7b09fde169e
Json087 <dalbello.yan@gmail.com>
2025-04-29 08:55:47 -0300

    Exemplo: contador + imagem

--- CARDMOB-main/etapa2/app-react-exemplos/App.js
+++ CARDMOB-main/etapa2/app-react-exemplos/App.js
@@ -1,11 +1,26 @@
+import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
+
 
 export default function App() {
+  const [counter, setcounter] = useState(0)
+  const i= () =>{
+    setcounter(counter + 1)
+  }
+  const d = () =>{
+    setcounter(counter - 1)
+  }
   return (
     <View style={styles.container}>
-      <Text>Ola!</Text>
+      <Text style={styles.text}>Counter: {counter}</Text>
+      {/* <Image source{URL:{'https://picsum.photos/200'}}/> */}
       <StatusBar style="auto" />
+
+      <View style={styles.buttonConteiner}>
+        <Button title='Decremento' onPress={d} />
+        <Button title='Incremento' onPress={i} />
+      </View>
     </View>
   );
 }
@@ -13,8 +28,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#262626',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+  },
+  text:{
+    fontSize: 40,
+    color: '#fff'
+  },
+  buttonConteiner:{
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
   },
 });
