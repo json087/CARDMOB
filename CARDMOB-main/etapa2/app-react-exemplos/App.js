d31257ac17e49d6d285d2b7685db4d81bdb17e4a
Json087 <dalbello.yan@gmail.com>
2025-04-29 09:29:55 -0300

    CRUD simples: addItem

--- CARDMOB-main/etapa2/app-react-exemplos/App.js
+++ CARDMOB-main/etapa2/app-react-exemplos/App.js
@@ -1,20 +1,43 @@
 import React, {useState} from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button} from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput} from 'react-native';
 
 
 export default function App() {
   const [counter, setcounter] = useState(0)
+
+  // crud
+  const [items,  setItems] = useState([])
+  const [text, setText]= useState('')
+  const [editItemId, setEditItemId] = useState(null)
+  const [editItemText, setEditItemText] = useState('')
+
   const i= () =>{
     setcounter(counter + 1)
   }
   const d = () =>{
     setcounter(counter - 1)
   }
+
+  const addItem = () =>{
+    if (text.trim()=== ''){
+      return
+    }
+    const newItem ={
+      id: Math.random().toString(),
+      text: text.trim()
+    }
+    setItems([...items, newItem]);
+    setText('');
+    console.log(items);
+    
+  }
   return (
     <View style={styles.container}>
+      <TextInput style={styles.input} value={text} placeholder='digite algo' onChangeText={setText}/>
+      <Button title='Add item' onPress={addItem}/>
       <Text style={styles.text}>Counter: {counter}</Text>
-      {/* <Image source{URL:{'https://picsum.photos/200'}}/> */}
+      {/* <Image source{URL{'https://picsum.photos/200'}}/> */}
       <StatusBar style="auto" />
 
       <View style={styles.buttonConteiner}>
